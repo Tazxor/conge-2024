c6cb435e022405a9c62a06df512af7606a96d54f
Tazxor <79080127+Tazxor@users.noreply.github.com>
2024-08-30 12:28:43 +0200

    12Commit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+// tests/ExampleTest.php
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testBasic()
+    {
+        $this->assertTrue(true);
+    }
+}
